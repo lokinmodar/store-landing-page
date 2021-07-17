38e4e867f9a02fb01c26f0c3fd4ff8e731326aba
Dante Souza e Souza <lokinmodar@gmail.com>
2021-07-17 16:55:39 -0300

    TS Boilerplate code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import { ReactComponent as Logo } from './logo.svg';
+import './App.scss';
 
-function App() {
+export default function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <section className='App'>
+      <header className='App-header'>
+        <Logo className='App-logo' title='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React?!
         </a>
       </header>
-    </div>
+    </section>
   );
 }
-
-export default App;
---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect } from 'react';
+
+export default function Main(): JSX.Element {
+  return (
+    <>
+      <h1>Hello!</h1>
+    </>
+  );
+}
